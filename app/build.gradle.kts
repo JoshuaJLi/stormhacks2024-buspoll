ddc74ac57e7f0f155517c4cf5cea66e8e32a4c95
Joshua Li <JoshuaJLi@outlook.com>
2024-10-05 13:16:05 -0700

    Remove compose.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,10 +47,6 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
-
-    buildFeatures {
-        compose = true
-    }
 }
 
 dependencies {
@@ -63,6 +59,10 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -70,45 +70,4 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-
-    val composeBom = platform("androidx.compose:compose-bom:2024.09.03")
-    implementation(composeBom)
-    androidTestImplementation(composeBom)
-
-    // Choose one of the following:
-    // Material Design 3
-    implementation(libs.material3)
-    // or Material Design 2
-    implementation(libs.androidx.material)
-    // or skip Material Design and build directly on top of foundational components
-    implementation(libs.androidx.foundation)
-    // or only import the main APIs for the underlying toolkit systems,
-    // such as input and measurement/layout
-    implementation(libs.ui)
-
-    // Android Studio Preview support
-    implementation(libs.ui.tooling.preview)
-    debugImplementation(libs.ui.tooling)
-
-    // UI Tests
-    androidTestImplementation(libs.ui.test.junit4)
-    debugImplementation(libs.ui.test.manifest)
-
-    // Optional - Included automatically by material, only add when you need
-    // the icons but not the material library (e.g. when using Material3 or a
-    // custom design system based on Foundation)
-    implementation(libs.androidx.material.icons.core)
-    // Optional - Add full set of material icons
-    implementation(libs.androidx.material.icons.extended)
-    // Optional - Add window size utils
-    implementation(libs.androidx.adaptive)
-
-    // Optional - Integration with activities
-    implementation(libs.androidx.activity.compose)
-    // Optional - Integration with ViewModels
-    implementation(libs.androidx.lifecycle.viewmodel.compose)
-    // Optional - Integration with LiveData
-    implementation(libs.androidx.runtime.livedata)
-    // Optional - Integration with RxJava
-    implementation(libs.androidx.runtime.rxjava2)
 }
