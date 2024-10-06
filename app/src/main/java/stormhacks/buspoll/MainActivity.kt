07ed7d29b65b4dfb88ddf9e2d0039b80b1bd8921
elc10 <ethan_chou@sfu.ca>
2024-10-05 18:29:01 -0700

    added an arraylist of stops

--- app/src/main/java/stormhacks/buspoll/MainActivity.kt
+++ app/src/main/java/stormhacks/buspoll/MainActivity.kt
@@ -1,13 +1,16 @@
 package stormhacks.buspoll
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 class MainActivity : ComponentActivity() {
+    private var stopList: ArrayList<Stop> = ArrayList<Stop>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,5 +20,19 @@ class MainActivity : ComponentActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val minput = InputStreamReader(assets.open("stops.csv"))
+        val reader = BufferedReader(minput)
+
+        var line : String?
+        var displayData : String = ""
+        while (reader.readLine().also { line = it } != null){
+            val row : List<String> = line!!.split(",")
+            var newStop = Stop(row[1], row[2], row[4], row[5])
+            stopList.add(newStop)
+            displayData = displayData + row[4] + "\t" + row[5] + "\n"
+        }
+
+        var txtData = findViewById(R.id.textView) as TextView
+        txtData.text = stopList.toString()
     }
 }
---
+++ app/src/main/java/stormhacks/buspoll/Stop.kt
@@ -0,0 +1,8 @@
+package stormhacks.buspoll
+
+data class Stop(
+    private var stopNumber: String,
+    private var stopName: String,
+    private var latitude: String,
+    private var longitude: String
+)
